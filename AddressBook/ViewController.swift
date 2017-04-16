c27b4b72ac90b860bd50710bb8281da54966f56a
Selase Kwawu <hiselase@gmail.com>
2017-04-16 21:55:05 +0000

    Initial Commit

---
+++ AddressBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AddressBook
+//
+//  Created by Selase Kwawu on 16/04/2017.
+//  Copyright © 2017 Selase Kwawu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
